15a7041e1fc4b4bfb999e399110a43fd52b1f944
Rafał Dołęga <rdolega@userengage.com>
2018-03-01 14:26:39 +0100

    add dependencies

--- app/src/main/java/com/userengage/mobile_sdk_demo/App.java
+++ app/src/main/java/com/userengage/mobile_sdk_demo/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
     private void initUserEngage() {
         UserEngage userEngage = new UserEngage.Builder()
                 .context(this)
-                .apiKey("123")
+                .apiKey("wUfNyt2BL21ooUEtKGkA5sG8hlevjvtVL8ZqBbmTSSoWffdK26j2JeFKeCnfeF2c")
+                .baseUrl("http://10.0.2.2:8000/") //use only if you have self hosted UE engine
                 .trackActivities()
                 .build();
     }
