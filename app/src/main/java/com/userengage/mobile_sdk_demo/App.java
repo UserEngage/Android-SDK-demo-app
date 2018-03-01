bb5b0884f8e7562b5696e197ea321511d46ee563
Rafał Dołęga <rdolega@userengage.com>
2018-03-01 17:44:47 +0100

    Add sample baseUrl and apiKey

--- app/src/main/java/com/userengage/mobile_sdk_demo/App.java
+++ app/src/main/java/com/userengage/mobile_sdk_demo/App.java
@@ -3,6 +3,12 @@ package com.userengage.mobile_sdk_demo;
 import android.app.Application;
 
 import com.userengage.userengagesdk.UserEngage;
+import com.userengage.userengagesdk.notifications.NotificationAction;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class App extends Application {
     @Override
@@ -17,9 +23,21 @@ public class App extends Application {
     private void initUserEngage() {
         UserEngage userEngage = new UserEngage.Builder()
                 .context(this)
-                .apiKey("wUfNyt2BL21ooUEtKGkA5sG8hlevjvtVL8ZqBbmTSSoWffdK26j2JeFKeCnfeF2c")
-                .baseUrl("http://10.0.2.2:8000/") //use only if you have self hosted UE engine
+                .apiKey("<api key generated in Userengage web panel>")
+                .baseUrl("https://app.userengage.com/api/sdk/v1/") //use only if you have self hosted UE engine
+                .notificationRoutes(getRoutesAction())
                 .trackActivities()
                 .build();
+    }
+
+    private static List<NotificationAction> getRoutesAction(){
+
+        Set<String> intentKeys = new HashSet<>();
+
+        List<NotificationAction> actionList = new ArrayList<>();
+
+        actionList.add(new NotificationAction(MainActivity.class, intentKeys));
+
+        return actionList;
     }
 }
